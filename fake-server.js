e8ae3dff1f4a26c271d54f3a0dd29653471cc5b0
Edouard <edouardbuilder87@gmail.com>
2019-11-28 14:05:13 -0500

    adding more rfid

--- fake-server.js
+++ fake-server.js
@@ -2,11 +2,19 @@ var socket = require('socket.io-client')('http://localhost:3000');
 const rfid = require("node-rfid");
 
 console.log("Reading...")
-rfid.read(function(err, result) {
+// rfid.read(function(err, result) {
 
-    if (err) {console.log("Some hardware error occured")};
-    console.log(results);
+//     if (err) {console.log("Some hardware error occured")};
+//     console.log(results);
 
+// });
+rfid.readintime(5000, function (err, result) {
+    if (err) console.log("Sorry, some hardware error occurred"); //some kind of hardware/wire error
+    if (result == "timeout") {
+        console.log("Sorry, You timed out"); //check if time exceeded the time you passed as argument and print timeout message
+    } else {
+        console.log(result); //print rfid tag UID
+    }
 });
 
 socket.on('connect', function () {
