54774966e1692af9045ec07b3e8f7e42886d69c2
Edouard <edouardbuilder87@gmail.com>
2019-11-28 23:41:53 -0500

    fixing errors

--- fake-server.js
+++ fake-server.js
@@ -1,32 +1,32 @@
-var socket = require('socket.io-client')('http://localhost:3000');
-const rfid = require("node-rfid");
+// var socket = require('socket.io-client')('http://localhost:3000');
+// const rfid = require("node-rfid");
 
-socket.on('connect', function () {
-    console.log("connected!")
-});
-socket.emit("scan", {
+// socket.on('connect', function () {
+//     console.log("connected!")
+// });
+// socket.emit("scan", {
 
-    rfid: 12345,
-    machine: 0
+//     rfid: 12345,
+//     machine: 0
 
-});
-socket.on("status", data => {
+// });
+// socket.on("status", data => {
 
-    if (data.status == "authorized") {
+//     if (data.status == "authorized") {
 
-        while (true) {
+//         while (true) {
 
-            if (Math.floor(new Date().getTime() / 1000) >= data.end) {
+//             if (Math.floor(new Date().getTime() / 1000) >= data.end) {
 
-                break;
+//                 break;
 
-            }
+//             }
 
-        }
+//         }
 
-        console.log("session expired")
-        // io.emit("expired")
+//         console.log("session expired")
+//         // io.emit("expired")
 
-    } 
+//     } 
 
-});
+// });
