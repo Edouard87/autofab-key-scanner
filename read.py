966dc75f1ecf83a402f4977d3219057d7044864d
Edouard <edouardbuilder87@gmail.com>
2020-04-12 01:01:15 -0400

    changed some code

--- read.py
+++ read.py
@@ -3,6 +3,7 @@ import sys
 from mfrc522 import SimpleMFRC522
 reader = SimpleMFRC522()
 
-id, text = reader.read()
-print((id,text))
-sleep(1)
+while True:
+    id, text = reader.read()
+    print(id, text)
+    sleep(1)
--- spawnPy.js
+++ spawnPy.js
@@ -13,5 +13,6 @@ let shell = new PythonShell('read.py', options);
 
 shell.on("message", message => {
     console.log(message)
+    console.log(message.split())
 })
 
